1bf36491bd14457c7262ae27f72f955336791d1f
dylan jones <dylanjones6@pm.me>
2024-07-28 09:40:42 -0600

    added good portion of handle_client message flow

--- src/structs.rs
+++ src/structs.rs
@@ -1,6 +1,6 @@
 use core::slice::SlicePattern;
 use std::net::{TcpStream, UdpSocket, Ipv4Addr};
-use std::io::{ErrorKind, Read, Result, Seek, Write};
+use std::io::{Error, ErrorKind, Read, Result, Seek, Write};
 use std::fs::File;
 use std::string::FromUtf8Error;
 use std::sync::{Mutex, Arc, RwLock};
@@ -49,12 +49,33 @@ enum Message {
 }
 
 impl Message {
-    fn send(&self, stream: Mutex<TcpStream>) -> Self {
-        let vec: Vec<u8> = match self {
+    fn new(command: u8,
+           data: u16,
+           message_length: u8,
+           message: Vec<u8>) -> Result<Self> {
+        match &command {
+            0 | 1 | 2 => {
+                Ok(Message::Short(command, data))
+            }
+            3 | 4 => {
+                Ok(Message::Long(command, message_length, message))
+            }
+            _ => {
+                eprintln!("Received command other than 0-4, in Message::new()):
+                          {}. Exiting now.", &command);
+                std::process::exit(1)
+            }
+        }
+    }
+    fn send(&self, stream: Arc<Mutex<TcpStream>>) -> Result<()> {
+        let buf: Vec<u8> = match self {
             Message::Short(command, data) => {
-                let mut vec = vec!(*command);
-                vec.push(data.to_be_bytes()[0]);
-                vec.push(data.to_be_bytes()[1]);
+                //let mut vec = vec!(*command);
+                //vec.push(data.to_be_bytes()[0]);
+                //vec.push(data.to_be_bytes()[1]);
+                let vec = vec!(*command,
+                               data.to_be_bytes()[0],
+                               data.to_be_bytes()[1]);
                 vec
             }
             Message::Long(command, message_length, message) => {
@@ -62,22 +83,102 @@ impl Message {
                 //vec.push(*message);
                 for i in message {
                     vec.push(*i);
-                vec
                 }
+                let vec: Vec<u8> = vec;
+                vec
             }
         };
+        stream.lock().unwrap().write_all(&buf);
+        stream.lock().unwrap().flush();
+        Ok(())
+    }
+    fn receive(stream: Arc<Mutex<TcpStream>>) -> Result<Self> {
+        let mut buf = Vec::new();
+        match stream.lock().unwrap().read_to_end(&mut buf) {
+            Ok(_n_bytes) => {
+                match &buf[0] {
+                    0 | 1 | 2 => {
+                        //Ok(Message::Short(buf[0],
+                        //               NetworkEndian::read_u16(&buf[1..3])))
+                        Message::new(buf[0],
+                                     NetworkEndian::read_u16(&buf[1..3]),
+                                     0,
+                                     Vec::new())
+                    }
+                    3 | 4 => {
+                        //Ok(Message::Long(buf[0],
+                        //              buf[1],
+                        //              buf[2..].to_vec()))
+                        Message::new(buf[0],
+                                     0,
+                                     buf[1],
+                                     buf[2..].to_vec())
+                    }
+                    _ => {
+                        eprintln!("Received command other than 0-4 in /
+                                   message::receive(): {}. Exiting /
+                                   now.", &buf[0]);
+                        std::process::exit(1)
+                    }
+                }
+            }
+            Err(error) => {
+                eprintln!("Error returned while reading TcpStream");
+                Err(error)
+            }
+        }
+    }
+    fn receive_and_expect(stream: Arc<Mutex<TcpStream>>,
+                          expected_command: u8) -> Result<Self> {
+        match Message::receive(stream) {
+            Ok(message) => {
+            }
+
+        }
     }
 }
 
-pub fn handle_client() -> Result<()> {
+pub fn handle_client(stream: Arc<Mutex<TcpStream>>) -> Result<()> {
     // 1. receive hello
 
-    receive_message()
-
+    match Message::receive(stream) {
+        Ok(Message::Short(command, data)) => {
+            if command == 0 {
+                //TODO save udp_port in data to some struct?
+            } else {
+                eprintln!("Received a non-hello short message in handshake, /
+                           exiting.");
+                std::process::exit(1)
+            }
+        }
+        Ok(Message::Long(command, message_length, message)) => {
+            eprintln!("Received a non-short message in handshake, exiting.");
+            eprintln!("{}, {}, {:?}", command, message_length, message);
+            std::process::exit(1)
+        }
+        Err(error) => {
+            eprintln!("Received an error while expecting hello: {}", error);
+            std::process::exit(1)
+        }
+    }
     // 2. save client_udp_port in local structure
+
+
+
     // 3. reply with the number of stations available in a welcome
+
+    let welcome = Message::new(2, song_path_vec_len, 0, Vec::new())?;
+    welcome.send(stream);
+
     // 4. start a loop to wait for set station with client
+
+    loop {
+        Message::receive(stream)
+
+    }
+
     // 5. receive a station number from client, and add udp port to a shareable
     //    struct that will be used in a udp_player loop
     // 6. 
+    Ok(())
 }
